09b56f48d205ba6f1b0e5c44dd7dac13dfae2708
tm.shih <tm.shih@advantech.com.tw>
2020-01-09 11:14:46 +0800

    Add example to load and render specified pdf.

--- app/src/main/java/com/test/pdfrenderer/MainActivity.java
+++ app/src/main/java/com/test/pdfrenderer/MainActivity.java
@@ -1,5 +1,11 @@
 package com.test.pdfrenderer;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.graphics.Rect;
+import android.graphics.pdf.PdfRenderer;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -7,12 +13,25 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.os.ParcelFileDescriptor;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 1000;
+    private final String TAG = getClass().getSimpleName() + "@" + Integer.toHexString(hashCode());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +48,9 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        checkAppPermissions();
+        initUI();
     }
 
     @Override
@@ -51,5 +73,81 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void checkAppPermissions() {
+        if (!isGranted(Manifest.permission.READ_EXTERNAL_STORAGE)
+                || !isGranted(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            // Permission is not granted, request to grant.
+            String[] permissions = {
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE};
+            ActivityCompat.requestPermissions(
+                    this,
+                    permissions,
+                    PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+        }
+    }
+
+    private boolean isGranted(String permission) {
+        return ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void initUI() {
+        final Button btnRendering = findViewById(R.id.btnRendering);
+        final EditText etFilePath = findViewById(R.id.etFilePath);
+        final EditText etPageIndex = findViewById(R.id.etPageIndex);
+        btnRendering.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String filePath = etFilePath.getText().toString();
+                int pageIndex = Integer.parseInt(etPageIndex.getText().toString());
+                render(filePath, pageIndex);
+            }
+        });
+    }
+
+    private void render(String filePath, int currentPage) {
+        try {
+            ImageView imgRenderArea = findViewById(R.id.imgRenderArea);
+
+            int REQ_WIDTH = 1;
+            int REQ_HEIGHT = 1;
+            REQ_WIDTH = imgRenderArea.getWidth();
+            REQ_HEIGHT = imgRenderArea.getHeight();
+            Log.i(TAG, "render(): " + filePath + " page=" + currentPage + " " + REQ_WIDTH + "x" + REQ_HEIGHT);
+
+            Bitmap bitmap = Bitmap.createBitmap(REQ_WIDTH, REQ_HEIGHT, Bitmap.Config.ARGB_4444);
+
+            File file = new File(filePath);
+            ParcelFileDescriptor fileDescriptor = ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_ONLY);
+            PdfRenderer renderer = new PdfRenderer(fileDescriptor);
+
+            if (currentPage < 0) {
+                currentPage = 0;
+            } else if (currentPage > renderer.getPageCount()) {
+                currentPage = renderer.getPageCount() - 1;
+            }
+
+            Matrix matrix = imgRenderArea.getImageMatrix();
+            matrix.setScale(.75f, .75f);
+            Rect rect = new Rect(0, 0, REQ_WIDTH, REQ_HEIGHT);
+
+            PdfRenderer.Page rendererPage = renderer.openPage(currentPage);
+            rendererPage.render(bitmap, rect, matrix, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY);
+
+            imgRenderArea.setImageMatrix(matrix);
+            imgRenderArea.setImageBitmap(bitmap);
+            imgRenderArea.invalidate();
+
+            /* Recycling of usage. */
+            rendererPage.close();
+            renderer.close();
+            fileDescriptor.close();
+        } catch (Exception e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+            Log.w(TAG, "render(): " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
